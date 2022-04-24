756d32f14c709cb0bd8515c231c1e411aaaff93b
abdullah-manafikhi <abdullahmanafikhi32@gmail.com>
2022-04-24 17:09:09 +0300

    you still have to edit the video display

--- assets/index.js
+++ assets/index.js
@@ -3,15 +3,25 @@ window.addEventListener("load" , () => {
     let clsBtn =  document.getElementById("projects__vid__cls");
     let video = document.querySelector(".projects__demo");
     let demos = document.querySelectorAll(".projects__demo__btn")
-
+    let sociel = document.querySelectorAll(".sociel i");
+    
+console.log(sociel)
     clsBtn.addEventListener("click" , () => {
         video.style.display = "none" ;
     })
 
-    demos.forEach(demo => { demo.addEventListener("click" , () =>{
+    demos.forEach(demo => { demo.addEventListener("click" , () =>   {
             video.style.display = "block" ;
         })
-    }
-    )
+    })
 
+    window.addEventListener("scroll" , xy)
+
+    function xy () {
+        setTimeout(() => {
+            console.log(sociel)
+          sociel.forEach((icon) => {icon.style.right = "0rem";} )
+        } , 2000)
+        window.removeEventListener("scroll" , xy)
+    }
 })  
