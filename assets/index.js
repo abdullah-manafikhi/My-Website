686d7a85fbcc966673c17f9748db7ba1c29da4a3
abdullah-manafikhi <abdullahmanafikhi32@gmail.com>
2022-09-08 23:21:43 +0300

    v-2.0.0

--- assets/index.js
+++ assets/index.js
@@ -14,18 +14,67 @@ window.addEventListener("load" , () => {
         var vidName = demo.getAttribute("rel")
         console.log(vidName)
         vid.src = `assets/videos/${vidName}.mp4` 
+        console.log(vid.getAttribute('src'))
         video.style.display = "block" ;
         vid.load();
         })
     })
 
-    window.addEventListener("scroll" , xy)
+    window.addEventListener("scroll" , displaySocialIcons)
 
-    function xy () {
+    function displaySocialIcons () {
         setTimeout(() => {
             console.log(sociel)
           sociel.forEach((icon) => {icon.style.right = "0rem";} )
         } , 2000)
-        window.removeEventListener("scroll" , xy)
+        window.removeEventListener("scroll" , displaySocialIcons)
     }
+
+    const reactTab = document.getElementById("react__tab")
+    const jsTab = document.getElementById("js__tab")
+    const jsProjects = document.getElementById("js__projects")
+    const reactProjects = document.getElementById("react__projects")
+    const projectsInitClass = reactProjects.className
+
+    reactTab.addEventListener('click' , () => {
+        console.log(reactProjects.style.display)
+
+        if(!reactTab.classList.contains("active")){
+            var tabInitClass = reactTab.className
+            reactTab.className += " active"
+            jsTab.className = tabInitClass
+            reactProjects.className = projectsInitClass
+            jsProjects.className += ' hidden'
+            console.log(reactProjects.className)
+            console.log(jsProjects.className)
+        }
+    })
+
+    jsTab.addEventListener('click' , () => {
+        console.log(reactProjects.className)
+        if(!jsTab.classList.contains("active")){
+            var tabInitClass = jsTab.className 
+            reactTab.className = tabInitClass
+            jsTab.className += " active"
+            jsProjects.className = projectsInitClass
+            reactProjects.className += ' hidden'
+            console.log(reactProjects.className)
+            console.log(jsProjects.className)
+
+
+        }
+        
+    })
+
+    ScrollReveal().reveal('.contact__hdr,.sec__head' , {duration: 1000 , distance : '10%' , origin: 'left' });
+    ScrollReveal().reveal('.animated__project,.about__p,.about__grid' , {duration: 1000 , scale:1.05 , delay: 500 });
+
+
+    const inputs = document.querySelectorAll('input')
+
+    inputs.forEach((input) => {
+        input.addEventListener('click' , () => {
+            alert("The contact me section is not working yet please contact me throw Gmail or Whats app")
+        })
+    })
 })  
